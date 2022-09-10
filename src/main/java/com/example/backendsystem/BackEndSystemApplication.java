62f02d257d4f96b8093dcf5281716b28d702db19
haha <1581128015@qq.com>
2022-09-10 12:59:24 +0800

    第一次 commit

---
+++ src/main/java/com/example/backendsystem/BackEndSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendsystem/BackEndSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
